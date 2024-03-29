152ea24792a16c42bcde05c6cf8c1b3f08c6a220
francksefu <francksefu1998@gmail.com>
2024-03-29 17:19:55 +0200

    Finish with header and description

---
+++ src/components/homes/Hearder.jsx
@@ -0,0 +1,20 @@
+
+const Header = () => {
+  return (
+    <div className="container-fluid header p-3">
+      <div className="ps-3 ms-3">
+        <p className="text-light pb-0 mb-0">
+            I can help you build a product , feature or website
+          </p>
+          <h1 className="text-light mt-0 pt-0">
+            I'm Franck Sefu <br/>
+            I am software developer
+          </h1>
+        
+      </div>
+      
+    </div>
+  );
+}
+
+export default Header;
